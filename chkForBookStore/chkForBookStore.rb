bb906726319d9a8bc32484e81f72604a56843178
yosukei3108 <yosukei3108@gmail.com>
2021-10-31 16:40:01 +0900

    fix urls and set depth_limit as 0

--- chkForBookStore/chkForBookStore.rb
+++ chkForBookStore/chkForBookStore.rb
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 require 'anemone'
 
-urls = [
-  "https://www.amazon.co.jp/gp/bestsellers/books/",
-  "https://www.amazon.co.jp/gp/bestsellers/digital-text/2275256051/"]
+# urls = [
+#   "https://www.amazon.co.jp/gp/bestsellers/books/",
+#   "https://www.amazon.co.jp/gp/bestsellers/digital-text/2275256051/"]
+
+urls = []
+urls.push("https://www.amazon.co.jp/gp/bestsellers/digital-text/2293263051/ref=zg_bs_nav_kinc_2_2275256051/355-3307941-7892730")
+urls.push("https://www.amazon.co.jp/gp/bestsellers/digital-text/2291657051/ref=zg_bs_nav_kinc_2_2275256051/355-3307941-7892730")
+urls.push("https://www.amazon.co.jp/gp/bestsellers/books/466298/ref=zg_bs_nav_b_1_b/356-1209185-7393605")
+urls.push("https://www.amazon.co.jp/gp/bestsellers/books/466290/ref=zg_bs_nav_b_1_b/356-1209185-7393605")
 
 # Anemone.crawl (urls) do |anemone|
-Anemone.crawl(urls, :depth_limit => 1, :skip_query_strings => true) do |anemone|
-# Anemone.crawl (urls, {:depth_limit => 0, :skip_query_strings => true,}) do |anemone|
+# Anemone.crawl(urls, :depth_limit => 1, :skip_query_strings => true) do |anemone|
+Anemone.crawl(urls, :depth_limit => 0) do |anemone|
 
 
   anemone.focus_crawl do |page|
