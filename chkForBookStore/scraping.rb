a586318376d3ce3c2e7f4c2ac92a44c14068f40e
yosukei3108 <yosukei3108@gmail.com>
2021-11-02 09:08:14 +0900

    get ASIN

--- chkForBookStore/scraping.rb
+++ chkForBookStore/scraping.rb
@@ -9,6 +9,13 @@ urls.push("https://www.amazon.co.jp/gp/bestsellers/digital-text/2291657051/ref=z
 urls.push("https://www.amazon.co.jp/gp/bestsellers/books/466298/ref=zg_bs_nav_b_1_b/356-1209185-7393605")
 urls.push("https://www.amazon.co.jp/gp/bestsellers/books/466290/ref=zg_bs_nav_b_1_b/356-1209185-7393605")
 
+puts "legend"
+puts "category / subcategory"
+puts
+puts "#Rank"
+puts "Title"
+puts "ASIN"
+
 Anemone.crawl(urls, :depth_limit => 0) do |anemone|
   anemone.on_every_page do |page|
 
@@ -24,22 +31,32 @@ Anemone.crawl(urls, :depth_limit => 0) do |anemone|
       "//*[@id='zg_browseRoot']/ul/ul/ul/li/span").text
 
     if category == "本"
+      puts "###############################################"
       puts category + " /" + sub_category_books
+      puts "###############################################"
     elsif category == "Kindleストア"
+      puts "###############################################"
       puts category + " /" + sub_category_digital_text
+      puts "###############################################"
     else
       puts "faild to find category"
     end
 
+    # <div class="a-section a-spacing-none aok-ralative">
     items = doc.xpath("//ol[@class=\"a-ordered-list a-vertical\"]/li/span/div")
 
     items.each {|item|
-    # oreder
+    # rank
     puts item.xpath("div/span/span").text
 
     # title
-    # puts item.xpath("span/a/div").text
-    puts item.xpath("span/a/span/div/img").attribute("alt").value
+    puts item.xpath("span/a/div").text.strip
+
+    # ASIN (Amazon Standard Identification Number)
+    puts item.xpath("span/a").attribute("href").text.match(%r{dp/(.+?)/})[1]
+
+    puts
   }
+  puts
   end
 end
